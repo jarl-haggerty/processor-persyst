d3a7eaa625e6728e37dc38a6c0ee15f56d4d84c8
Rohan Shah <rohanmalavshah@gmail.com>
2024-04-18 15:40:57 -0400

    reformat

--- processor/edf_processor/edf.py
+++ processor/edf_processor/edf.py
@@ -4,35 +4,35 @@ import re
 import numpy as np
 from datetime import datetime
 
-second_to_usecond = 1000000
+SECOND_TO_USECOND = 1000000
 
-def twosComp(val, bits):
-    """compute the 2's complement of int value val"""
-    if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
-        val = val - (1 << bits)        # compute negative value
-    return val                         # return positive value as is
+def twos_comp(val, bits):
+    """Compute the 2's complement of int value val."""
+    if (val & (1 << (bits - 1))) != 0:  # If sign bit is set, e.g., 8bit: 128-255
+        val = val - (1 << bits)         # Compute negative value
+    return val                          # Return positive value as is
 
-def transformValue(val, phys_min, phys_max, dig_min, dig_max, bits):
-    two_comped_val = twosComp(val, bits)
-    bit_value = (phys_max - phys_min) /(dig_max - dig_min)
+def transform_value(val, phys_min, phys_max, dig_min, dig_max, bits):
+    two_comped_val = twos_comp(val, bits)
+    bit_value = (phys_max - phys_min) / (dig_max - dig_min)
     offset = (phys_max / bit_value) - dig_max
     return bit_value * (offset + float(two_comped_val))
 
-class EdfReader():
+class EdfReader:
     """
-    Simple interface for EDF, EDF+C and EDF+D files
+    Simple interface for EDF, EDF+C, and EDF+D files.
     """
     
-    def __init__ (self,filename):
+    def __init__ (self, filename):
         self.open(filename)
     
     def __enter__(self):
         return self
 
-    def __exit__(self,exception_type, exception_value, traceback):
+    def __exit__(self, exception_type, exception_value, traceback):
         return
 
-    def open(self,filename):
+    def open(self, filename):
         with open(filename, "rb") as f:
             self.version = f.read(8).strip().decode('utf-8', 'ignore')
             self.patient_id = f.read(80).strip().decode('utf-8', 'ignore')
@@ -73,57 +73,57 @@ class EdfReader():
                         block = f.read(2 * self.nr_samples[x])
                         segment = [block[i:i + 2] for i in range(0, len(block), 2)]
                         self.data_signal[x] = np.append(self.data_signal[x], [
-                            transformValue(struct.unpack('<H', z)[0], self.phy_min[x], self.phy_max[x], self.dig_min[x],
+                            transform_value(struct.unpack('<H', z)[0], self.phy_min[x], self.phy_max[x], self.dig_min[x],
                                             self.dig_max[x], 16) for z in segment])
     
-    def getNSamples(self):
-        return [x* self.nb_data_rec for x in self.nr_samples] 
+    def get_n_samples(self):
+        return [x * self.nb_data_rec for x in self.nr_samples] 
 
-    def getSignalLabels(self):
+    def get_signal_labels(self):
         return self.labels
    
-    def getRecordStartTime(self,i):
+    def get_record_start_time(self, i):
         return self.rec_start_time[i]
     
-    def getNumberOfDataRecords(self):
+    def get_number_of_data_records(self):
         return self.nb_data_rec
 
-    def getNrSamples(self,i):
+    def get_nr_samples(self, i):
         return self.nr_samples[i]
 
-    def getSampleFrequency(self, i):
+    def get_sample_frequency(self, i):
         return float(self.nr_samples[i]) / self.duration
 
-    def getPhysicalDimension(self,i):
+    def get_physical_dimension(self, i):
         return self.phy_dim[i]
 
-    def getStartdatetime(self):
-        split_start_date=self.start_date.split(".")
-        split_start_time=self.start_time.split(".")
-        year=int(split_start_date[2])+1900
-        if (year<1985):
-            year=year+100
-        month=int(split_start_date[1])
-        day=int(split_start_date[0])
-        hour=int(split_start_time[0])
-        minute=int(split_start_time[1])
-        second=int(split_start_time[2])
+    def get_start_datetime(self):
+        split_start_date = self.start_date.split(".")
+        split_start_time = self.start_time.split(".")
+        year = int(split_start_date[2]) + 1900
+        if (year < 1985):
+            year = year + 100
+        month = int(split_start_date[1])
+        day = int(split_start_date[0])
+        hour = int(split_start_time[0])
+        minute = int(split_start_time[1])
+        second = int(split_start_time[2])
         return datetime(year, month, day, hour, minute, second)
 
-    def getTimestamps(self, index, signal_number, start_time) :
-        #start_time is in usecs sine epoch
-        start_time_record = start_time + self.getRecordStartTime(index) * second_to_usecond  
-        end_time_record = start_time_record + second_to_usecond * self.getDuration() 
-        return np.linspace(start_time_record, end_time_record, num=self.getNrSamples(signal_number), endpoint=False) 
+    def get_timestamps(self, index, signal_number, start_time) :
+        # Start_time is in usecs since epoch
+        start_time_record = start_time + self.get_record_start_time(index) * SECOND_TO_USECOND  
+        end_time_record = start_time_record + SECOND_TO_USECOND * self.get_duration() 
+        return np.linspace(start_time_record, end_time_record, num=self.get_nr_samples(signal_number), endpoint=False) 
   
-    def isDiscontiguous(self):
+    def is_discontiguous(self):
         return (self.reserved == "EDF+D")
 
-    def getDuration(self):
+    def get_duration(self):
         return self.duration
     
-    def getAnnotations(self):
+    def get_annotations(self):
         return self.annotations
 
-    def readSignal(self, i, start,end):
+    def read_signal(self, i, start, end):
         return self.data_signal[i][start:end]
--- processor/edf_processor/processor.py
+++ processor/edf_processor/processor.py
@@ -1,68 +1,65 @@
+import traceback
 from base_processor.timeseries import BaseTimeSeriesProcessor
 from base_processor.timeseries.base import chunks
 import base_processor.timeseries.utils as utils
 from edf_processor.edf import EdfReader
-import sys
 
 class EdfProcessor(BaseTimeSeriesProcessor):
 
     def task(self):
-        file = self.inputs['file']
+        file_path = self.inputs['file']
         try:
-            edf_file = EdfReader(file)
-            # sample size for all signals
-            n_samples = edf_file.getNSamples()
+            with EdfReader(file_path) as edf_file:
+                n_samples = edf_file.get_n_samples()
 
-            # add channels/segments for each signal
-            for i, signal_name in enumerate(edf_file.getSignalLabels()):
-                samples   = n_samples[i]
-                sample_rate = edf_file.getSampleFrequency(i)
-                length = (samples - 1) / sample_rate
-                start_time_date_obj = edf_file.getStartdatetime().replace(tzinfo=None)
-                start_time = utils.usecs_since_epoch(start_time_date_obj)
-                end_time = int(start_time + length * 1e6)
+                for i, signal_name in enumerate(edf_file.get_signal_labels()):
+                    if signal_name == 'EDF Annotations':
+                        continue
 
-                unit = edf_file.getPhysicalDimension(i)
+                    sample_rate = edf_file.get_sample_frequency(i)
+                    unit = edf_file.get_physical_dimension(i)
 
-                if (signal_name == 'EDF Annotations'):
-                    #ignore the annotation channel"
-                    continue
-                elif (edf_file.isDiscontiguous()):
-                    #For EDF+D, we need to treat each "record" of *duration* seconds separately and use the time 
-                    #stamp for each record from the Annotation Channel (wrt to EDF file format specification)
-                    for index in range(edf_file.getNumberOfDataRecords()):
+                    if edf_file.is_discontiguous():
+                        for index in range(edf_file.get_number_of_data_records()):
+                            record_timestamps = edf_file.get_timestamps(index, i, utils.usecs_since_epoch(edf_file.get_start_datetime()))
 
-                        record_timestamps=edf_file.getTimestamps(index, i, start_time)
-                        nb_samples_per_record = edf_file.getNrSamples(i)
+                            nb_samples_per_record = edf_file.get_nr_samples(i)
 
-                        channel = self.get_or_create_channel(
-                            name = str(signal_name).strip(),
-                            unit = str(unit),
-                            rate = sample_rate,
-                            type = 'continuous')
-                       
-                        vals = edf_file.readSignal(i, index * nb_samples_per_record, (index+1) * nb_samples_per_record )
+                            channel = self.get_or_create_channel(
+                                name=str(signal_name).strip(),
+                                unit=str(unit),
+                                rate=sample_rate,
+                                type='continuous'
+                            )
 
-                        self.write_channel_data(
-                            channel = channel,
-                            timestamps = record_timestamps[0:len(vals)],
-                            values = edf_file.readSignal(i, index * nb_samples_per_record, (index+1) * nb_samples_per_record )
-                        )
-                else:                    
-                    for chunk in chunks(start_time,end_time, samples):
-                        # create channel object
-                        channel = self.get_or_create_channel(
-                            name = str(signal_name).strip(),
-                            unit = str(unit),
-                            rate = sample_rate,
-                            type = 'continuous')
+                            vals = edf_file.read_signal(i, index * nb_samples_per_record, (index + 1) * nb_samples_per_record)
 
-                        vals=edf_file.readSignal(i,chunk.start_index,chunk.end_index)
-                        self.write_channel_data(
-                            channel = channel,
-                            timestamps = chunk.timestamps[0:len(vals)],
-                            values = edf_file.readSignal(i,chunk.start_index,chunk.end_index)
-                        )
+                            self.write_channel_data(
+                                channel=channel,
+                                timestamps=record_timestamps[:len(vals)],
+                                values=vals
+                            )
+                    else:
+                        start_time = utils.usecs_since_epoch(edf_file.get_start_datetime())
+                        length = (n_samples[i] - 1) / sample_rate
+                        end_time = int(start_time + length * 1e6)
+
+                        for chunk in chunks(start_time, end_time, n_samples[i]):
+                            channel = self.get_or_create_channel(
+                                name=str(signal_name).strip(),
+                                unit=str(unit),
+                                rate=sample_rate,
+                                type='continuous'
+                            )
+
+                            vals = edf_file.read_signal(i, chunk.start_index, chunk.end_index)
+
+                            self.write_channel_data(
+                                channel=channel,
+                                timestamps=chunk.timestamps[:len(vals)],
+                                values=vals
+                            )
         except Exception as e:
             print(traceback.format_exc())
+
         self.finalize()
